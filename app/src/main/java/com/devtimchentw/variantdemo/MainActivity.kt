768a3a40337e9cc3eb3fe866c03166744cad814c
TimChen <devtimchentw@gmail.com>
2020-05-28 21:33:01 +0800

    feat: 對不同環境新增 applicationId 的區別與 BuildConfig 的資料區別

--- app/src/main/java/com/devtimchentw/variantdemo/MainActivity.kt
+++ app/src/main/java/com/devtimchentw/variantdemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.devtimchentw.variantdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showBanner = "showBanner is ${BuildConfig.SHOW_BANNER}"
+
+        tv_message.text = showBanner
     }
 }
